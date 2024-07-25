d1ca2276b14f4dcca48de4432f3b788b3717724e
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-07-25 10:55:48 -0300

    extends for mentoria and curso

--- Desafio_Abstrair_Dominio/src/main/java/dio/challenge/me/Mentoria.java
+++ Desafio_Abstrair_Dominio/src/main/java/dio/challenge/me/Mentoria.java
@@ -1,29 +1,15 @@
 package dio.challenge.me;
 
 import java.time.LocalDate;
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo{
     private LocalDate data;
 
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20d;
+    }
+
     // get and set
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     public LocalDate getData() {
         return data;
     }
@@ -35,8 +21,8 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
