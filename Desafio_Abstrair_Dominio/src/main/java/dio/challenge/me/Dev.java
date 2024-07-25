f44479d0880381b468c4d441596fa595eeb9a1d8
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-07-25 11:43:14 -0300

    create methods

--- Desafio_Abstrair_Dominio/src/main/java/dio/challenge/me/Dev.java
+++ Desafio_Abstrair_Dominio/src/main/java/dio/challenge/me/Dev.java
@@ -1,19 +1,34 @@
 package dio.challenge.me;
 
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class Dev {
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp){}
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-    public void progredir(){}
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
 
-    public void calcularTotalXP(){}
+
+    public double calcularTotalXP(){
+
+        return this.conteudosConcluidos.stream().
+                mapToDouble(conteudo -> conteudo.calcularXP())
+                .sum();
+    }
 
     //get e set
 
